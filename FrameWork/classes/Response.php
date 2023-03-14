ea0338bae6075885a4d53fe0a3e7853273e74415
Remko5 <100160435+Remko5@users.noreply.github.com>
2023-03-14 13:07:16 +0100

    header class aangemaakt + aanpassingen in request en response classes

--- FrameWork/classes/Response.php
+++ FrameWork/classes/Response.php
@@ -2,34 +2,55 @@
 
 class Response
 {
-    private $statusCode;
-    private $headers;
-    private $body;
+    private int $statusCode;
+    private array $headers;
+    private string $body;
+    private const ALLOWED_STATUS_CODES = [
+        100 => "Continue",
+        200 => "OK",
+        201 => "Created",
+        204 => "No Content",
+        301 => "Moved Permanently",
+        302 => "Found",
+        303 => "See Other",
+        307 => "Temporary Redirect",
+        308 => "Permanent Redirect",
+        400 => "Bad Request",
+        401 => "Unauthorized",
+        403 => "Forbidden",
+        404 => "Not Found",
+        418 => "I'm  a teapot",
+        500 => "Internal Server Error",
+        503 => "Service Unavailable"
+    ];
 
-
-
-    public function __construct($body,$header=[], $statusCode=200)
+    public function __construct(string $body, array $header=[], int $statusCode=200)
     {
-        $this-> statusCode = $statusCode;
-        $this-> headers = $header;
-        $this-> body =$body;
+        $this->statusCode = $statusCode;
+        $this->headers = $header;
+        $this->body =$body;
     }
 
-    public function addHeader($name, $value){
+    public function addHeader(string $name, string $value): void
+    {
         $this->headers[$name][] = $value;
     }
 
-    public function setHeader($name, $value){
-        $this->headers[$name] = [(String) $value];
+    public function setHeader(string $name, string $value): void
+    {
+        $this->headers[$name] = [$value];
     }
 
+    public function changeStatusCode(int $statusCode): void
+    {
+        if(in_array($statusCode, array_keys(self::ALLOWED_STATUS_CODES)))
+        {
+            $this->statusCode = $statusCode;
+        }
+    }
 
+    public function send(): void
+    {
+        var_dump($this);
+    }
 }
-
-
-
-
-
-
-
-
