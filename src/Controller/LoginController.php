fab316b0ae840b4a211d5f2f2bf9e0abd8dc47da
Ihab <safadi_i@yahoo.com>
2023-04-12 21:58:36 +0200

    doc

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace src\Controller;
 
 
+use FrameWork\HTTP\isRequest;
 use FrameWork\HTTP\Request;
 use src\Model\Rol;
 use src\Model\User;
@@ -12,26 +13,29 @@ class LoginController
 
     public function loginUser(Request $request)
     {
-        //De
+        //Deceleratie voor variabelen die later worden gebruiken om de gegevens van de gebruiken op te slaan.
         $sesstionId = null;
         $sesstionUsername = null;
 
-        if ($request->getServer()['REQUEST_METHOD'] === 'POST') {
+        //check eerst of er een post request is.
+        if (isRequest::post()){
 
+            //haal de data van de form
             $email = $request->getPostByName('username');
             $password = $request->getPostByName('password');
 
+            //haal de gegevens van de gebruiker vanuit de database
             $getUserCred = User::find(['email' => $email]);
-
             $findRole =  Rol::find(['id' => $getUserCred->getRolId()]);
 
 
-
-
+            // Verificatie of het wachtwoord correct is met wat in de database staat.
+            // Als er geen sprake van een just wachtwoord of email dan weiger de inlog, anders login en start een sessie en sla de gegevens van de gebruikers op.
         if (!$getUserCred || !password_verify($password, $getUserCred->getPassword())) {
             echo '<div>Incorrect username or password.</div>';
             header('Location: /login');
         } else {
+
             // Als de inloggegevens geldig zijn dan begin een session
             session_start();
             $_SESSION['user_id'] = $getUserCred->getId();
@@ -41,8 +45,6 @@ class LoginController
             $sesstionId = $_SESSION['user_id'];
             $sesstionUsername = $_SESSION['user_Name'];
             header('Location: /home');
-
-
 
         }
         }
@@ -56,9 +58,9 @@ class LoginController
 
     public function logout()
     {
+        // Destroy de sessie wanneer de gebruiker op 'logout' drukt.
         session_unset();
         session_destroy();
-
         return[];
     }
 
