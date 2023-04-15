67645f034527d392c18f77a3f51b25178cecb81d
Remko5 <100160435+Remko5@users.noreply.github.com>
2023-04-16 01:29:10 +0200

    vergeten toe te voegen...

--- FrameWork/Database/DatabaseConnection.php
+++ FrameWork/Database/DatabaseConnection.php
@@ -1,32 +1,36 @@
 <?php
+
 namespace FrameWork\database;
+
 use Exception;
-use PDO;
-use PDOException;
 
 class DatabaseConnection {
     private string $host = "localhost";
     private string $username = "Ihab";
     private string $password = "Welkom01!";
     private string $dbname = "web2project";
-    private PDO $connector;
+    private object $connector;
 
     /**
      * @throws Exception
      */
-    public function __construct()
+    public function __construct(private string $connectorClass = "PDO",
+                                private string $exceptionClass = "PDOException")
     {
         $dsn = "mysql:host=$this->host;dbname=$this->dbname";
-        $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+        $options = array($this->connectorClass::ATTR_ERRMODE => $this->connectorClass::ERRMODE_EXCEPTION, $this->connectorClass::ATTR_DEFAULT_FETCH_MODE => $this->connectorClass::FETCH_ASSOC);
 
         try {
-            $this->connector = new PDO($dsn, $this->username, $this->password, $options);
-        } catch (PDOException $e) {
-            throw new Exception("contact met database is niet gelukt: " . $e->getMessage());
+            $this->connector = new $this->connectorClass($dsn, $this->username, $this->password, $options);
+        } catch (Exception $e) {
+            echo match (get_class($e)) {
+                $this->exceptionClass => throw new $this->exceptionClass("contact met database is niet gelukt: " . $e->getMessage()),
+                default => throw new Exception("contact met database is niet gelukt: " . $e->getMessage()),
+            };
         }
     }
 
-    public function getConnector(): PDO
+    public function getConnector(): object
     {
         return $this->connector;
     }
